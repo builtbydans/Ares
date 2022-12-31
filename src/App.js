c6d96fdc904bc93dbe3cad3a5d545be672f89b02
Danish Shafi <danish.shafi@live.co.uk>
2022-12-31 02:13:51 +0000

    Header and TopSelling components added to project

--- src/App.js
+++ src/App.js
@@ -1,46 +1,12 @@
-import Button from 'react-bootstrap/Button';
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Form from 'react-bootstrap/Form';
-import Navbar from 'react-bootstrap/Navbar';
-import BrandLogo from '../src/images/ares_logo.png'
 import './App.css';
+import Header from './components/Header'
+import TopSelling from './components/TopSelling';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      <Navbar expand="lg">
-      <Container>
-        <Navbar.Brand href="#">
-         <img src={BrandLogo} alt="brand-logo" width={100} />
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="navbarScroll" />
-        <Navbar.Collapse id="navbarScroll">
-          <Nav
-            className="me-auto my-2 my-lg-0"
-            style={{ maxHeight: '100px' }}
-            navbarScroll
-          >
-            <Nav.Link href="#action1">Home</Nav.Link>
-            <Nav.Link href="#action2">Link</Nav.Link>
-            <Nav.Link href="#" disabled>
-              Link
-            </Nav.Link>
-          </Nav>
-          <Form className="d-flex">
-            <Form.Control
-              type="search"
-              placeholder="Search"
-              className="me-2"
-              aria-label="Search"
-            />
-            <Button variant="outline-success">Search</Button>
-          </Form>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-      </header>
+      <Header />
+      <TopSelling />
     </div>
   );
 }
