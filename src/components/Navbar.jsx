fad2b0c9c6027f1514df3bc4a6dd97f2ea6d04ec
builtbydans <danish.shafi@live.co.uk>
2025-10-02 14:43:12 +0100

    product card v1 complete

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,18 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
-export default function Navbar() {
+const Navbar = () => {
+  const [isHovered, setIsHovered] = useState(false);
+
   return (
-    <nav className="flex items-center justify-between bg-white shadow px-6 py-4">
-      <Link to="/" className="text-2xl font-bold text-blue-600">
-        ARES
+    <nav className="flex items-center justify-between px-6 py-4">
+      <Link
+        onMouseEnter={() => setIsHovered(true)}
+        onMouseLeave={() => setIsHovered(false)}
+        to="/"
+        className="text-2xl font-bold text-black manrope"
+      >
+        {isHovered ? "アレス" : "ARES"}
       </Link>
       <div className="flex gap-4">
         <Link to="/cart" className="hover:text-blue-600">
@@ -16,4 +24,6 @@ export default function Navbar() {
       </div>
     </nav>
   );
-}
+};
+
+export default Navbar;
